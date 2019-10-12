03a0aa0ea827e8857cce029fe5a077159b1c39d6
natasha2358 <karacanakova.natasa@gmail.com>
2019-10-12 18:54:44 +0200

    proba merge

---
+++ EventReservation/EventReservation/Controllers/LocalsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EventReservation.Models;
+
+namespace EventReservation.Controllers
+{
+    public class LocalsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Locals
+        public ActionResult Index()
+        {
+            return View(db.Locals.ToList());
+        }
+
+        // GET: Locals/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Local local = db.Locals.Find(id);
+            if (local == null)
+            {
+                return HttpNotFound();
+            }
+            return View(local);
+        }
+
+        // GET: Locals/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Locals/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,City,StreetName,StreetNo,OpeningHour,ClosingHour,NoTables,Parking")] Local local)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Locals.Add(local);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(local);
+        }
+
+        // GET: Locals/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Local local = db.Locals.Find(id);
+            if (local == null)
+            {
+                return HttpNotFound();
+            }
+            return View(local);
+        }
+
+        // POST: Locals/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,City,StreetName,StreetNo,OpeningHour,ClosingHour,NoTables,Parking")] Local local)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(local).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(local);
+        }
+
+        // GET: Locals/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Local local = db.Locals.Find(id);
+            if (local == null)
+            {
+                return HttpNotFound();
+            }
+            return View(local);
+        }
+
+        // POST: Locals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Local local = db.Locals.Find(id);
+            db.Locals.Remove(local);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
