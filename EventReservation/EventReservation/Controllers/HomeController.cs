06efd3a056668f01c7827e9dc1cf756b376da96d
Kristina Andonovska <kristina.andonovska@outlook.com>
2020-01-18 18:27:40 +0100

    "home and contact pages edited"

--- EventReservation/EventReservation/Controllers/HomeController.cs
+++ EventReservation/EventReservation/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EventReservation.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -20,11 +23,25 @@ namespace EventReservation.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Contact(LocalRequest model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
         }
     }
 }
